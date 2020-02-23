933d317fa4a0108bc52a3a0bb06a6ad17d246ca2
ML <54767542+malijuly@users.noreply.github.com>
2020-02-23 22:02:33 +0100

    Add files via upload

---
+++ planner_v3/planner_v3/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace planner_v3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button_next = new System.Windows.Forms.Button();
+            this.button_prev = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.btn_today = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(669, 73);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "01.01.2000";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btn_today);
+            this.panel2.Controls.Add(this.button_next);
+            this.panel2.Controls.Add(this.button_prev);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(481, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(188, 73);
+            this.panel2.TabIndex = 1;
+            // 
+            // button_next
+            // 
+            this.button_next.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button_next.Location = new System.Drawing.Point(123, 0);
+            this.button_next.Name = "button_next";
+            this.button_next.Size = new System.Drawing.Size(65, 73);
+            this.button_next.TabIndex = 2;
+            this.button_next.Text = ">";
+            this.button_next.UseVisualStyleBackColor = true;
+            this.button_next.Click += new System.EventHandler(this.button_next_Click);
+            // 
+            // button_prev
+            // 
+            this.button_prev.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button_prev.Location = new System.Drawing.Point(0, 0);
+            this.button_prev.Name = "button_prev";
+            this.button_prev.Size = new System.Drawing.Size(61, 73);
+            this.button_prev.TabIndex = 1;
+            this.button_prev.Text = "<";
+            this.button_prev.UseVisualStyleBackColor = true;
+            this.button_prev.Click += new System.EventHandler(this.button_prev_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.Location = new System.Drawing.Point(156, 79);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(501, 676);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label13);
+            this.panel3.Controls.Add(this.label12);
+            this.panel3.Controls.Add(this.label11);
+            this.panel3.Controls.Add(this.label10);
+            this.panel3.Controls.Add(this.label9);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(11, 79);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(117, 676);
+            this.panel3.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 42);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "9:00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(2, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 42);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "10:00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(3, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 42);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "11:00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(12, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 42);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "8:00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(1, 230);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 42);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "12:00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(1, 287);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 42);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "13:00";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(2, 339);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(112, 42);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "14:00";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(1, 397);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(112, 42);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "15:00";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.Location = new System.Drawing.Point(1, 455);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(112, 42);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "16:00";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label11.Location = new System.Drawing.Point(2, 516);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(112, 42);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "17:00";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label12.Location = new System.Drawing.Point(1, 572);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(112, 42);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "18:00";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label13.Location = new System.Drawing.Point(1, 626);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(112, 42);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "20:00";
+            // 
+            // btn_today
+            // 
+            this.btn_today.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_today.Location = new System.Drawing.Point(61, 0);
+            this.btn_today.Name = "btn_today";
+            this.btn_today.Size = new System.Drawing.Size(62, 73);
+            this.btn_today.TabIndex = 4;
+            this.btn_today.Text = "TODAY";
+            this.btn_today.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 756);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button_next;
+        private System.Windows.Forms.Button button_prev;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_today;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ planner_v3/planner_v3/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace planner_v3
+{
+    public partial class Form1 : Form
+    {
+        DateTime thisDay = DateTime.Today;
+        List<RichTextBox> listka = new List<RichTextBox>();
+
+        public RichTextBox RichTextBox { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+            label1.Text = thisDay.ToString("d");
+
+          
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button_prev_Click(object sender, EventArgs e)
+        {
+            thisDay = thisDay.AddDays(-1);
+            label1.Text = thisDay.ToString("d");
+            richTextBox1.Text = "";
+        }
+
+        private void button_next_Click(object sender, EventArgs e)
+        {
+            
+            thisDay = thisDay.AddDays(1);
+            label1.Text = thisDay.ToString("d");
+            richTextBox1.Text = "";
+           
+        
+        }
+
+        private void btn_today_Click(object sender, EventArgs e)
+        {
+            DateTime thisDay = DateTime.Today;
+            label1.Text = thisDay.ToString("d");
+
+        }
+    }
+}
